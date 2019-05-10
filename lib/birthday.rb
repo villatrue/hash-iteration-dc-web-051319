787dd9f22c40b54e105118bb06a2f0177a4fb740
Sean Carter <scarter0313@gmail.com>
2019-05-10 23:14:30 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -16,6 +16,8 @@ def age_appropriate_birthday(birthday_kids)
    |kids_name, age|
    if age <= 12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  else
+    puts "You are too old for this"
   end
   end
 end
