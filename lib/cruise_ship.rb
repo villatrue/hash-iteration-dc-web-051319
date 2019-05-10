924b8e690a6e36a8eaa5dc5a4fdf49dffb8952ba
Sean Carter <scarter0313@gmail.com>
2019-05-10 23:00:10 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,13 +1,7 @@
 
 
 def select_winner(passengers)
-  passengers = {
-suite_a: "Amanda Presley", 
-suite_b: "Seymour Hoffman", 
-suite_c: "Alfred Tennyson", 
-suite_d: "Charlie Chaplin", 
-suite_e: "Crumpet the Elf"
-}
+  
 winner = ""
 passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
