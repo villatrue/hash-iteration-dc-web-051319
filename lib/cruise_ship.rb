6f323f3442ee94931dd26393b3bc7717a4029644
Sean Carter <scarter0313@gmail.com>
2019-05-10 22:59:35 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,13 @@ suite_b: "Seymour Hoffman",
 suite_c: "Alfred Tennyson", 
 suite_d: "Charlie Chaplin", 
 suite_e: "Crumpet the Elf"
-}# add the code snippet here!
+}
+winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
+end
+ 
+winner
 end
