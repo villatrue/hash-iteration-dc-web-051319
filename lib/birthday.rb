c91e84fa037ea0dd059560d03d27778a7d30c26d
Sean Carter <scarter0313@gmail.com>
2019-05-10 23:12:37 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,7 +7,14 @@
 def happy_birthday(birthday_kids)
  birthday_kids.each do
    |kids_name, age|
-   if age <= 12
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
+
+def age_appropriate_birthday(birthday_kids)
+ birthday_kids.each do
+   |kids_name, age|
+   if age <= 7
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
