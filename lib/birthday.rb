fc7e11988995d2ca2f981a02ed10799dd24c6920
Sean Carter <scarter0313@gmail.com>
2019-05-10 23:05:51 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,7 +7,13 @@
 def happy_birthday(birthday_kids)
  birthday_kids.each do
    |kids_name, age|
-    puts "Happy Birthday #{kids_name} You are now #{age} years old!"
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
 end
 
 
+# def happy_birthday(birthday_kids)
+#   birthday_kids.each do |kids_name, age|
+#     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+#   end
+# end
